a929a6245b546c2dc3601ae7dbc09c33f0948370
Pawel Cempura <pawelcempura07@gmail.com>
2024-04-09 17:17:57 +0200

    first frontend config

---
+++ frontend/src/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@nextui-org/react";
+
+export default function Page() {
+
+  return (
+    <div>
+      <Button radius="full" className="bg-gradient-to-tr from-pink-500 to-yellow-500 text-white shadow-lg">
+      Button
+      </Button>
+    </div>
+  );
+}
