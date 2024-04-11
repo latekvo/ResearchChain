26fe13f744012c13e9cb09d502a1b4aee9d47222
pawelcp <pawelcempura07@gmail.com>
2024-04-11 22:12:33 +0200

    New svg background

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,10 @@
 import PromptyInput from "./components/PromptInput";
+import BackgroundSvg from "./components/Background";
 
 export default function Page() {
   return (
-      <section className="w-screen h-screen flex justify-center items-center bg-circle-purple bg-cover bg-center">
+      <section className="w-screen min-h-screen flex justify-center items-center bg-circle-purple bg-cover bg-center">
+        <BackgroundSvg></BackgroundSvg>
         <PromptyInput />
       </section>
   );
