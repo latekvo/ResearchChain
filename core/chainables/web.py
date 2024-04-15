b4800ccf134a1d148c3e21e7f55f810133e6c5f7
LatekVon <latkaignacy@gmail.com>
2024-04-15 22:55:37 +0200

    quicksave before large reformatting

--- core/chainables/web.py
+++ core/chainables/web.py
@@ -6,79 +6,92 @@ from langchain_core.prompts import ChatPromptTemplate
 
 
 def web_news_lookup(prompt_text: str):
-    query = WebQuery('news', prompt_core=prompt_text)
+    query = WebQuery("news", prompt_core=prompt_text)
     return web_query_google_lookup(query)
 
 
 def web_wiki_lookup(prompt_text: str):
-    query = WebQuery('wiki', prompt_core=prompt_text)
+    query = WebQuery("wiki", prompt_core=prompt_text)
     return web_query_google_lookup(query)
 
 
 def web_docs_lookup(prompt_text: str):
-    query = WebQuery('docs', prompt_core=prompt_text)
+    query = WebQuery("docs", prompt_core=prompt_text)
     return web_query_google_lookup(query)
 
 
 def web_docs_lookup_prompt():
-    return ChatPromptTemplate.from_messages([
-        (
-            "system",
-            "You are a search results interpreter."
-            "Your job is to write an detailed instruction based on the provided context. "
-            "Your job is to convert all the search results you were given into a long, comprehensive and clean output. "
-            "Use provided search results data to explain object of user request to the best of your ability. "
-            "You don't have a knowledge cutoff. "
-            "It is currently " + datetime.date.today().strftime("%B %Y"),
-        ),
-        (
-            "user",
-            "Search results data: "
-            "```"
-            "{search_data}"
-            "```"
-            'User request: "Write an article on: {user_request}"',
-        ),
-    ])
+    return ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a search results interpreter."
+                "Your job is to write an detailed instruction based on the provided context. "
+                "Your job is to convert all the search results you were given into a long, "
+                "comprehensive and clean output. "
+                "Use context data to explain "
+                "the topic of user request to the best of your ability. "
+                "You don't have a knowledge cutoff. "
+                "It is currently " + datetime.date.today().strftime("%B %Y"),
+            ),
+            (
+                "user",
+                "Search results data: "
+                "```"
+                "{search_data}"
+                "```"
+                'User request: "Write an article on: {user_request}"',
+            ),
+        ]
+    )
 
 
 def web_wiki_lookup_prompt():
-    return ChatPromptTemplate.from_messages([
-        (
-            "system",
-            "You are a search results interpreter. Your job is to write an article based on the provided context. "
-            "Your job is to convert all the search results you were given into a long, comprehensive and clean output. "
-            "Use provided search results data to answer the user request to the best of your ability. "
-            "You don't have a knowledge cutoff. "
-            "It is currently " + datetime.date.today().strftime("%B %Y"),
-        ),
-        (
-            "user",
-            "Search results data: "
-            "```"
-            "{search_data}"
-            "```"
-            'User request: "Write an article on: {user_request}"',
-        ),
-    ])
+    return ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a search results interpreter. "
+                "Your job is to write an article based on the provided context. "
+                "Your job is to convert all the search results you were given into a long, "
+                "comprehensive and clean output. "
+                "Use context data to answer "
+                "the user request to the best of your ability. "
+                "You don't have a knowledge cutoff. "
+                "It is currently " + datetime.date.today().strftime("%B %Y"),
+            ),
+            (
+                "user",
+                "Search results data: "
+                "```"
+                "{search_data}"
+                "```"
+                'User request: "Write an article on: {user_request}"',
+            ),
+        ]
+    )
 
 
 def web_news_lookup_prompt():
-    return ChatPromptTemplate.from_messages([
-        (
-            "system",
-            "You are a search results interpreter. Your job is to write an article based on the provided context. "
-            "Your job is to convert all the search results you were given into a long, comprehensive and clean output. "
-            "Use provided search results data to answer the user request to the best of your ability. "
-            "You don't have a knowledge cutoff. "
-            "It is currently " + datetime.date.today().strftime("%B %Y"),
-        ),
-        (
-            "user",
-            "Search results data: "
-            "```"
-            "{search_data}"
-            "```"
-            'User request: "Write an article on: {user_request}"',
-        ),
-    ])
+    return ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a search results interpreter. "
+                "Your job is to write an article based on the provided context. "
+                "Your job is to convert all the search results you were given into a long, "
+                "comprehensive and clean output. "
+                "Use provided context to answer the user request to the best of your ability. "
+                "You don't have a knowledge cutoff. "
+                "It is currently " + datetime.date.today().strftime("%B %Y"),
+            ),
+            (
+                "user",
+                "Search results data: "
+                "```"
+                "{search_data}"
+                "```"
+                'User request: "Write an article on: {user_request}"',
+            ),
+        ]
+    )
