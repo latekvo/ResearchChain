a73fd8d09b48d842eeff2936bbc47be9a11a26ee
Nekxis <95935590+Nekxis@users.noreply.github.com>
2024-04-15 22:17:28 +0200

    @nekxis/interpret prompt mode (#15)
    Co-authored-by: Ignacy Łątka <74246391+LatekVo@users.noreply.github.com>

--- core/lookup.py
+++ core/lookup.py
@@ -1,15 +1,19 @@
-import datetime
-
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.runnables import RunnableLambda
 from core.tools.utils import purify_name
 from core.models.configurations import use_configuration
+from langchain_core.prompts import ChatPromptTemplate, BaseChatPromptTemplate
 
 # TODO: replace with puppeteer, this one gets blocked occasionally
 from googlesearch import search
 
-from core.chainables.web import web_docs_lookup, web_wiki_lookup, web_news_lookup
+from core.chainables.web import (
+    web_docs_lookup,
+    web_wiki_lookup,
+    web_news_lookup,
+    web_docs_lookup_prompt,
+    web_news_lookup_prompt,
+    web_wiki_lookup_prompt)
 from core.tools.dbops import get_db_by_name
 from core.tools.model_loader import load_model
 
@@ -29,26 +33,6 @@ def web_chain_function(prompt_dict: dict):
     # TODO: news searches should strictly search for news fresher than 1 month / 1 week
     # TODO: news crawling should be done through only sites like medium, which are much more dense than google
     # TODO: create a different function + prompt for documentation / facts searching, and make this one news focused
-    web_interpret_prompt = ChatPromptTemplate.from_messages(
-        [
-            (
-                "system",
-                "You are a search results interpreter. Your job is to write an article based on the provided context. "
-                "Your job is to convert all the search results you were given into a long, comprehensive and clean output. "
-                "Use provided search results data to answer the user request to the best of your ability. "
-                "You don't have a knowledge cutoff. "
-                "It is currently " + datetime.date.today().strftime("%B %Y"),
-            ),
-            (
-                "user",
-                "Search results data: "
-                "```"
-                "{search_data}"
-                "```"
-                'User request: "Write an article on: {user_request}"',
-            ),
-        ]
-    )
 
     def get_user_prompt(_: dict):
         return prompt_dict["input"]
@@ -63,6 +47,16 @@ def web_chain_function(prompt_dict: dict):
         else:
             return web_docs_lookup(user_prompt)
 
+    def interpret_prompt_mode():
+        if prompt_dict["mode"] == "News":
+            return web_news_lookup_prompt()
+        elif prompt_dict["mode"] == "Docs":
+            return web_docs_lookup_prompt()
+        elif prompt_dict["mode"] == "Wiki":
+            return web_wiki_lookup_prompt()
+        else:
+            return web_docs_lookup_prompt()
+    web_interpret_prompt_mode = interpret_prompt_mode()
     # NOTE: a detour has been performed here, more details:
     #       web_chain_function will soon become just a tool playing a part of a larger mechanism.
     #       prompt creation will be taken over by prompt sentiment extractor which will extract all researchable
@@ -76,7 +70,7 @@ def web_chain_function(prompt_dict: dict):
             # this has to be a RunnableLambda, it cannot be a string
             "user_request": RunnableLambda(get_user_prompt),
         }
-        | web_interpret_prompt
+        | web_interpret_prompt_mode
         | llm
         | output_parser
     )
