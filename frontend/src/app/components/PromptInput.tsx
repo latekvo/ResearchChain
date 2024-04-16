ca880e76b9b74996f8fe3364d1970369a4bd1022
pawelcp <pawelcempura07@gmail.com>
2024-04-16 21:34:40 +0200

    second input idea

--- frontend/src/app/components/PromptInput.tsx
+++ frontend/src/app/components/PromptInput.tsx
@@ -5,46 +5,20 @@ import { Button, Input, Tab, Tabs } from "@nextui-org/react";
 
 function PromptyInput() {
   return (
-    <div className="w-1/2 h-1/4 p-5 rounded-2xl flex flex-col justify-between backdrop-blur-md bgcolor bg-black/50 drop-shadow-xl">
+    <div className="w-1/2 h-1/4 p-5 shadow-xl bg-black bg-opacity-45 z-10 flex flex-col justify-between">
       <Input
-        label="Topic of research:"
-        isClearable
-        radius="lg"
-        classNames={{
-          label: "text-black/50 dark:text-white/90 text-lg",
-          input: [
-            "text-2xl",
-            "bg-transparent",
-            "text-black/90 dark:text-white/90",
-            "placeholder:text-default-700/50 dark:placeholder:text-white/60",
-          ],
-          innerWrapper: "bg-transparent",
-          inputWrapper: [
-            "self-center",
-            "h-20",
-            "shadow-xl",
-            "bg-default-200/50",
-            "dark:bg-default/60",
-            "backdrop-blur-xl",
-            "backdrop-saturate-200",
-            "hover:bg-default-200/70",
-            "dark:hover:bg-default/70",
-            "group-data-[focused=true]:bg-default-200/70",
-            "dark:group-data-[focused=true]:bg-default/60",
-            "!cursor-text",
-          ],
-        }}
+        variant="underlined"
         placeholder="Type to search..."
+        color="secondary"
+        size="lg"
+        className="text-gray-300 px-3"
       />
-      <div className="w-full flex justify-between">
+      <div className="w-full mt-4 flex justify-between">
         <Tabs
           size="lg"
           aria-label="Options"
-          classNames={{
-            tabList:
-              "shadow-xl dark:text-white/90 bg-default-200/70 dark:bg-default/70 backdrop-blur-xl backdrop-saturate-200",
-            tabContent: "text-black/90 dark:text-white/90",
-          }}
+          variant="underlined"
+          color="secondary"
         >
           <Tab key="news" title="News" />
           <Tab key="docs" title="Docs" />
@@ -54,7 +28,8 @@ function PromptyInput() {
           <Button
             size="lg"
             type="submit"
-            className="ml-4 shadow-xl bg-default-200/70 dark:bg-default/70 backdrop-blur-xl backdrop-saturate-200 hover:bg-default-200/70 dark:hover:bg-default/70"
+            color="default"
+            variant="bordered"
             // endContent={<SearchIcon className="text-black/50 mb-0.5 dark:text-white/90 text-slate-400 pointer-events-none flex-shrink-0" />}
           >
             Search
