685e2d25f3dd8708bc77624e36552a3bcc74a4f8
LatekVon <latkaignacy@gmail.com>
2024-04-29 02:30:26 +0200

    add internal embedding db endpoints

--- core/databases/db_embeddings.py
+++ core/databases/db_embeddings.py
@@ -1,8 +1,36 @@
-from core.tools.utils import use_faiss
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
-vector_db = use_faiss("embeddings", "placeholder")
+from core.models.configurations import use_configuration
+from core.tools.model_loader import load_model
+from core.tools.utils import use_faiss, is_text_junk, remove_characters
 
-# this global db has to actually be a set of multiple
-# separate dbs, each associated with its own embed model
+llm_config, embedder_config = use_configuration()
+_, embedder = load_model()
 
-# this file will be populated in a separate PR, along with an embedding server
+vector_db = use_faiss("embeddings", embedder_config.model_name)
+
+text_splitter = RecursiveCharacterTextSplitter(
+    separators=embedder_config.buffer_stops,
+    chunk_size=embedder_config.model_token_limit,
+    chunk_overlap=embedder_config.chunk_overlap,
+    keep_separator=False,
+    strip_whitespace=True,
+)
+
+
+def db_add_text_batch(text: str, source_uuid: str):
+    # automatically splits text before embedding it
+    chunks = text_splitter.split_text(text)
+
+    for chunk in chunks:
+        if is_text_junk(chunk.page_content):
+            chunks.remove(chunk)
+            continue
+
+        chunk.page_content = remove_characters(chunk.page_content, ["\n", "`"])
+        chunk.page_content = chunk.page_content
+
+    if len(chunks) != 0:
+        vector_db.add_texts(documents=chunks, embeddings=embedder)
+
+    pass
