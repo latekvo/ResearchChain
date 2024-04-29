b9243b55d70c919be45d54874f00d0e95fe632ec
LatekVon <latkaignacy@gmail.com>
2024-04-29 02:31:17 +0200

    add initial embedding loop

--- workers/embedder.py
+++ workers/embedder.py
@@ -1,13 +1,17 @@
 # embedding worker
 # url_db -> THIS -> THIS.vector_db
+from colorama import Fore
 from tinydb import Query
+from tinydb.table import Document
 
-from core.databases import db_url_pool, defaults
+from core.databases import db_url_pool, db_embeddings
+from core.models.configurations import use_configuration
 
-PLACEHOLDER_EMBEDDER_NAME = "placeholder"
+rapid_queue_limit = 40
+rapid_queue: list[Document] = []
 
-url_queue_limit = 40
-url_rapid_queue = []
+# llm will be used - passed to an agentic preprocessor
+llm_config, embedder_config = use_configuration()
 
 # important note to remember for later
 # we can avoid the ... issue if we just lock the db for every transaction,
@@ -15,15 +19,26 @@ url_rapid_queue = []
 
 
 def processing_iteration():
-    url_queue_remaining_space = url_queue_limit - len(url_rapid_queue)
-    if url_queue_remaining_space < url_queue_limit:
-        db_url_pool.db_get_not_embedded(PLACEHOLDER_EMBEDDER_NAME)
+    global rapid_queue
+
+    url_queue_remaining_space = rapid_queue_limit - len(rapid_queue)
+    if url_queue_remaining_space < rapid_queue_limit:
+        rapid_queue += db_url_pool.db_get_not_embedded(embedder_config.model_name)
+
+    for url_object in rapid_queue:
+        print(url_object.values)
+        document = url_object.text
+        document_uuid = url_object.parent_uuid
+
+        db_embeddings.db_add_text_batch(document, document_uuid)
+
+    print(f"{Fore.CYAN}Document vectorization completed.{Fore.RESET}")
 
 
 processing_iteration()
 while True:
     db_query = Query()
-    db_not_embedded = db_url_pool.db_get_not_embedded(PLACEHOLDER_EMBEDDER_NAME)
+    db_not_embedded = db_url_pool.db_get_not_embedded(embedder_config.model_name)
     db_total = db_url_pool.db.all()
 
     print("urls left to be embedded:", len(db_not_embedded))
