304638ede1ff29f988912dd298450f4220e7a2ca
LatekVon <latkaignacy@gmail.com>
2024-05-20 00:58:46 +0200

    add task release mechanism

--- core/databases/db_completion_tasks.py
+++ core/databases/db_completion_tasks.py
@@ -57,7 +57,8 @@ def db_get_incomplete_completion_tasks(amount: int = 1):
 
 
 def db_release_executing_tasks(uuid_list: list[str]):
-    pass
+    fields = Query()
+    db.update({"executing": False}, fields.uuid in uuid_list)
 
 
 def db_update_completion_task_after_summarizing(summary: str, uuid: str):
