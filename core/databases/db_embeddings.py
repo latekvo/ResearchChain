1df6197a1d834c0c4f86a1e2e869afa1075637fb
LatekVon <latkaignacy@gmail.com>
2024-05-23 13:55:13 +0200

    cleanup

--- core/databases/db_embeddings.py
+++ core/databases/db_embeddings.py
@@ -2,7 +2,7 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 from core.models.configurations import load_llm_config
 from core.tools.model_loader import load_model
-from core.tools.utils import use_faiss, is_text_junk, remove_characters
+from core.tools.utils import use_faiss, is_text_junk
 
 llm_config, embedder_config = load_llm_config()
 _, embedder = load_model()
