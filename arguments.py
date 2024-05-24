4303fe437854ed2064e840cae3235cec274bce56
LatekVon <latkaignacy@gmail.com>
2024-05-24 12:08:33 +0200

    finish get_config function and move functional code to lower scope

--- arguments.py
+++ arguments.py
@@ -1,4 +1,5 @@
 import argparse
+from dataclasses import dataclass
 
 from core.models.configuration_objects.embedder_configuration import (
     EmbedderConfiguration,
@@ -7,12 +8,15 @@ from core.models.configuration_objects.llm_configuration import LlmConfiguration
 
 parser = argparse.ArgumentParser()
 
+# todo: llm and embedder choices should not be fixed,
+#       there should be an option to specify a custom config
 parser.add_argument(
-    "-M",
+    "-L",
     "--llm-model",
     type=str,
     dest="llm_choice",
     choices=[
+        "none",
         "ollama_medium",
         "ollama_small",
         "ollama_large",
@@ -21,14 +25,15 @@ parser.add_argument(
         "hugging_face_large",
     ],
     default="ollama_medium",
-    help="Select model configuration",
+    help="Select llm model configuration",
 )
 parser.add_argument(
-    "-M",
+    "-E",
     "--embedder-model",
     type=str,
-    dest="embedder_choice",
+    dest="embed_choice",
     choices=[
+        "none",
         "ollama_medium",
         "ollama_small",
         "ollama_large",
@@ -37,15 +42,20 @@ parser.add_argument(
         "hugging_face_large",
     ],
     default="ollama_medium",
-    help="Select model configuration",
+    help="Select embedding model configuration",
 )
+
+# use config specified either by one of the configuration files or by a custom path
+# idea behind this: user can either configure their workers by editing the config files,
+#                   or by providing their own configs. One of these methods can be deleted,
+#                   once we determine which is more useful for dockerized applications
 parser.add_argument(
     "-w",
     "--run-worker",
     type=str,
     dest="worker_type",
     choices=[
-        "none",  # legacy runner, todo: to be removed
+        "none",  # legacy runner / custom worker path
         "crawler",
         "embedder",
         "summarizer",
@@ -53,34 +63,59 @@ parser.add_argument(
     default="none",
     help="Select worker to run",
 )
+parser.add_argument(
+    "-c",
+    "--custom-worker-path",
+    type=str,
+    dest="worker_config_path",
+    default="none",
+    help="Specify worker config to run",
+)
 
 args = parser.parse_args()
 
-llm_path = "core/models/configurations/llm/{}.json".format(args.llm_choice)
-embed_path = "core/models/configurations/embeder/{}.json".format(args.embedder_choice)
-llm_config = LlmConfiguration(llm_path)
-embedder_config = EmbedderConfiguration(embed_path)
 
-# hf or ollama (compatibility)
-llm_supplier = llm_config.supplier
-embedder_supplier = embedder_config.supplier
+@dataclass
+class RuntimeConfiguration:
+    worker_type: str
+    worker_config_path: str
+    llm_config_name: str
+    embedder_config_name: str
+    llm_config: LlmConfiguration
+    embedder_config: EmbedderConfiguration
 
-if llm_supplier != embedder_supplier:
+
+def get_runtime_config():
+    llm_path = "core/models/configurations/llm/{}.json".format(args.llm_choice)
+    embed_path = "core/models/configurations/embeder/{}.json".format(args.embed_choice)
+
+    llm_config = LlmConfiguration(llm_path)
+    embedder_config = EmbedderConfiguration(embed_path)
+
+    # hf or ollama (compatibility)
+    llm_supplier = llm_config.supplier
+    embedder_supplier = embedder_config.supplier
+
     # todo: we should actually allow for this behaviour, should be a simple fix as well
-    raise ValueError(
-        "LLM and EMBEDDER suppliers differ, please use the same supplier for both."
+    if llm_supplier != embedder_supplier:
+        raise ValueError(
+            "LLM and EMBEDDER suppliers differ, please use the same supplier for both."
+        )
+
+    worker_config_path = "configs/{}.json".format(args.worker_type)
+
+    # todo: load one of the standard configs based on worker_type, and overlay flags atop
+    return RuntimeConfiguration(
+        worker_type=args.worker_type,
+        worker_config_path=worker_config_path,
+        llm_config_name=args.llm_choice,
+        embedder_config_name=args.embed_choice,
+        llm_config=llm_config,
+        embedder_config=embedder_config,
     )
 
+
+# todo: this is a legacy method of distributing config, remove ASAP
 LLM_CHOICE = args.llm_choice
-EMBEDDER_CHOICE = args.embedder_choice
-USE_HUGGING_FACE = llm_supplier == "hugging_face"
-
-
-def get_running_config():
-    return {
-        "worker_type": args.worker_type,
-        "llm_config_name": args.llm_choice,
-        "embedder_config_name": args.embedder_choice,
-        "llm_config": llm_config,
-        "embedder_config": embedder_config,
-    }
+EMBEDDER_CHOICE = args.embed_choice
+USE_HUGGING_FACE = False  # temporarily disabled HF
