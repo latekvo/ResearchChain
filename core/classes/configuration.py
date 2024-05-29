6ef94c9a86e6e4d884e80af602d3b5eaa0a3d2e6
LatekVon <latkaignacy@gmail.com>
2024-05-29 22:31:20 +0200

    cleanup

--- core/classes/configuration.py
+++ core/classes/configuration.py
@@ -13,9 +13,6 @@ from core.models.configuration_objects.embedder_configuration import (
 from core.models.configuration_objects.llm_configuration import LlmConfiguration
 
 
-# this is a serializable singleton global configuration file
-
-
 # global config, storage and caching object
 @dataclass
 class RuntimeConfiguration:
@@ -23,8 +20,10 @@ class RuntimeConfiguration:
     worker_config_path: str
     llm_config_name: str
     embedder_config_name: str
-    llm_config: LlmConfiguration
-    embedder_config: EmbedderConfiguration
+
+    # set at runtime for now
+    llm_config: LlmConfiguration = None
+    embedder_config: EmbedderConfiguration = None
     llm_object: Ollama | Llama = None
     embedder_object: OllamaEmbeddings | Llama = None
 
@@ -34,14 +33,11 @@ def load_runtime_config_from_file(path: str):
         data = json.load(f)
         config = RuntimeConfiguration(
             worker_type=data["worker_type"],
-            worker_config_path=data["worker_config_path"],
+            worker_config_path=path,
             llm_config_name=data["llm_config_name"],
             embedder_config_name=data["embedder_config_name"],
-            # vvv - check if these save correctly, if not, remove
-            llm_config=data["llm_config"],
-            embedder_config=data["embedder_config"],
         )
 
-        # todo: move logic regarding x_objects and x_configs here?
+        # todo: maybe move logic regarding x_objects and x_configs here?
 
     return config
